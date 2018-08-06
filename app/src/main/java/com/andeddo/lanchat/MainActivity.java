b0162eb9e479c0912679d6e65ad9b514912b3c37
不死焱炎 <hgj19950619@gmail.com>
2018-08-06 15:34:18 +0800

    增加主页UI

--- app/src/main/java/com/andeddo/lanchat/MainActivity.java
+++ app/src/main/java/com/andeddo/lanchat/MainActivity.java
@@ -2,12 +2,20 @@ package com.andeddo.lanchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        btn_login = findViewById(R.id.btn_login);
     }
 }
