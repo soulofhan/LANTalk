4615df8b854fa6e078aaf88b166dc618f9fa3114
不死焱炎 <hgj19950619@gmail.com>
2018-08-07 17:24:00 +0800

    更新界面UI

--- app/src/main/java/com/andeddo/lanchat/MainActivity.java
+++ app/src/main/java/com/andeddo/lanchat/MainActivity.java
@@ -3,10 +3,12 @@ package com.andeddo.lanchat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_login;
+    private EditText main_ip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void init(){
         btn_login = findViewById(R.id.btn_login);
+        main_ip = findViewById(R.id.main_ip);
     }
+
+
 }
