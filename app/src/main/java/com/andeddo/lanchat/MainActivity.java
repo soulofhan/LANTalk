3d713af15dec7d6a32a18b75d079c8596c4ea35c
不死焱炎 <hgj19950619@gmail.com>
2018-09-07 18:00:35 +0800

    添加禁止状态栏下拉

--- app/src/main/java/com/andeddo/lanchat/MainActivity.java
+++ app/src/main/java/com/andeddo/lanchat/MainActivity.java
@@ -1,11 +1,20 @@
 package com.andeddo.lanchat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.andeddo.lanchat.unit.customViewGroup;
+
+
+public class MainActivity extends Activity {
     private String IPAdress = "192.168.0.101";
     private int PORT = 30000;
 
@@ -14,11 +23,22 @@ public class MainActivity extends AppCompatActivity {
 
     private String link_IP;
 
+
+    customViewGroup view;
+    WindowManager manager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        disablePullNotificationTouch();
     }
 
     private void init() {
@@ -30,5 +50,41 @@ public class MainActivity extends AppCompatActivity {
         return et_ipAdress.getText().toString();
     }
 
+    private void disablePullNotificationTouch() {
+        manager = ((WindowManager) getApplicationContext()
+                .getSystemService(Context.WINDOW_SERVICE));
+        WindowManager.LayoutParams localLayoutParams = new WindowManager.LayoutParams();
+        localLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+        localLayoutParams.gravity = Gravity.TOP;
+        localLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
 
+                // this is to enable the notification to recieve touch events
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+
+                // Draws over status bar
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+
+        localLayoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+        localLayoutParams.height = (int) (25 * getResources()
+                .getDisplayMetrics().scaledDensity);
+        localLayoutParams.format = PixelFormat.RGBX_8888;
+        view = new customViewGroup(this);
+        manager.addView(view, localLayoutParams);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    //允许下拉
+    private void allowDropDown(){
+        manager.removeView(view);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        allowDropDown();
+    }
 }
--- app/src/main/java/com/andeddo/lanchat/SocketLink.java
+++ app/src/main/java/com/andeddo/lanchat/SocketLink.java
@@ -1,23 +1,44 @@
 package com.andeddo.lanchat;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class SocketLink {
-    private String IPAddress;
+    private String HOST;
     private int PORT;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
 
     /**
      *
      * @param IPAddress 传入连接的IP地址
-     * @param PORT 传入连接的端口号
+     * @param iPORT 传入连接的端口号
      */
-    public SocketLink(String IPAddress,int PORT){
-        this.IPAddress = IPAddress;
-        this.PORT = PORT;
+    public SocketLink(String IPAddress,int iPORT){
+        HOST = IPAddress;
+        PORT = iPORT;
     }
 
     private void connect(){
-        Socket mSocket = new Socket();
+
+        try {
+            Socket mSocket = new Socket();
+            mSocket.setSoTimeout(3000);
+            mSocket.connect(new InetSocketAddress(HOST, PORT), 10000);                      //设置超时为10秒
+
+            bufferedReader = new BufferedReader(new InputStreamReader(
+                    mSocket.getInputStream()));
+
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(
+                    mSocket.getOutputStream(), "utf-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
